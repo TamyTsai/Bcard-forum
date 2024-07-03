d430f7ebfe2d2e714d0d0faeebcac2898dfb6512
Tamy <tamy8677@gmail.com>
2024-07-03 14:33:39 +0800

    controller 404 not found, view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    # 捕捉到ActiveRecord::RecordNotFound exception時，就使用record_not_found方法
+
+    private
+    
+    def record_not_found
+        render file: "#{Rails.root}/public/404.html", # 讀取404頁面
+               status: :not_found,
+               layout: false # 不套用預設的application layout
+    end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -55,5 +55,11 @@ class PostsController < ApplicationController
 
     def find_post
         @post = current_user.posts.find(params[:id])
+        # find找不到時會噴exception ActiveRecord::RecordNotFound
+        # 可用bigin resue局部處理 或是提升到controller層級
+        # begin
+        #     @post = current_user.posts.find(params[:id])
+        # rescue => exception
+        # end
     end
 end
