fca36f2c59278ae685496ba8061bc58879a567bb
Tamy <tamy8677@gmail.com>
2024-07-03 12:39:43 +0800

    post index action view, time zone setting, edit user registration view, post edit action view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,13 @@ class PostsController < ApplicationController
     before_action :authenticate_user!
     # 後面還可以接only 或 except
 
+    before_action :find_post, only: [:edit, :update, :destroy]
+    # rails 7.1後only except裡面不能寫還沒寫的action，但可以改變設定：`config.action_controller.raise_on_missing_callback_actions` to `false`.
+
+    def index
+        @posts = current_user.posts.order(created_at: :desc)
+    end
+
     def new # 建立文章表單頁面 對應的動作
         # @post = Post.new
         # 於user model 設定 has_many :posts 後可以寫成
@@ -16,14 +23,32 @@ class PostsController < ApplicationController
         @post = current_user.posts.new(post_params)
 
         if @post.save
-            redirect_to posts_path, notice: '新增成功'
+            redirect_to posts_path, notice: '文章新增成功'
         else
             render :new
         end
     end
 
+    def edit
+    end
+    
+    def update
+        if @post.update(post_params)
+            redirect_to posts_path, notice: '文章更新成功'
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+    end
+
     private
     def post_params # 資料清洗
         params.require(:post).permit(:title, :content)
+    end
+
+    def find_post
+        @post = current_user.posts.find(params[:id])
     end
 end
