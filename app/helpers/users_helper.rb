50ae11b2ec00b05eaf559f3660b391781a4cd9f6
Tamy <tamy8677@gmail.com>
2024-07-04 15:32:56 +0800

    flow control—show username（ID）, cardname : post edit, post index, registration edit, profile-left

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
 
     def avatar(user, size = 200)
         # image_tag user.avatar.variant(resize_to_fit: [size, size]), class: 'user-avatar' if user.avatar.attached?
-        image_tag user.avatar.variant(resize_to_fill: [size, size]), class: 'avatar my-5' if user.avatar.attached?
+        image_tag user.avatar.variant(resize_to_fill: [size, size]), class: 'avatar' if user.avatar.attached?
         # 將影像縮放至填滿指定尺寸，同時保留原始長寬比。必要時，將在較大的尺寸上裁剪影像。
     end
 
