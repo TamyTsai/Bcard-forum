2d0a6e40b7cabdd02547a97e4473863cb88de72b
Tamy <tamy8677@gmail.com>
2024-07-05 15:40:12 +0800

    yarn add tailwindcss, init

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,4 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers" // 會去找controllers下的index.js
+// 路徑是資料夾 且 沒有檔名 就會去找該資料夾下的 index檔案（webpack慣例）
