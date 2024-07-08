f99de863d8d5714923a966f8c2d6d31f2cdd3110
Tamy <tamy8677@gmail.com>
2024-07-08 09:29:27 +0800

    user view follow function ajax

--- app/javascript/controllers/user_controller.js
+++ app/javascript/controllers/user_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import axios from 'axios'
 
 export default class extends Controller {
-    static targets = [ "followButton" ]
+    static targets = [ "followButton", "followButtonBig" ]
 
 
     follow(event) {
@@ -46,5 +46,45 @@ export default class extends Controller {
                 console.log(error)
             })
 
+    }
+
+    followBig(event) {
+        event.preventDefault();
+
+        let user = event.currentTarget.dataset.user
+        // data: { action: "user#followBig", user: @post.user.id }
+
+        let target = this.followButtonBigTarget
+        // 要被變動顯示內容 的html元素
+
+        axios.post(`/api/v1/users/${user}/follow`)
+        // follow_api_v1_user    POST   /api/v1/users/:id/follow(.:format)       api/v1/users#follow
+            .then(function(response){
+                
+                let status = response.data.status 
+                
+                // 後端controller（users#follow）傳過來的資料
+                // render json: {status: '已取消追蹤'} 或 render json: {status: '已追蹤'}
+                switch (status) {
+                    case '使用者未登入':
+                    // 後端controller： render json: {status: '使用者未登入'} 
+                        alert('先登入才能追蹤該用戶喔');
+                        break;
+                    case '已取消追蹤':
+                        target.innerHTML = '追蹤'; // innerHTML對html元素內容的置換可以及時顯示，不用重新整理頁面
+                        // alert('已取消追蹤');
+                        target.classList.add('bgc-light-blue', 'hover:bg-sky-400', 'text-white');
+                        target.classList.remove('bg-gray-300', 'hover:bg-white');
+                        break;
+                    case '已追蹤':
+                        target.innerHTML = '追蹤中';
+                        target.classList.add('bg-gray-300', 'hover:bg-white');
+                        target.classList.remove('bgc-light-blue', 'hover:bg-sky-400', 'text-white');
+                        break;
+                }
+            })
+            .catch(function(error){
+                console.log(error)
+            })
     }
 }
