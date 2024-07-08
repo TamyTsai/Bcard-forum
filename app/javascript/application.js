eb20ecb45ffe369bd4ac8c80daf880c00cc69957
Tamy <tamy8677@gmail.com>
2024-07-08 15:12:12 +0800

    yarn add froala-editor, import its js and css

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,5 @@
 import "@hotwired/turbo-rails"
 import "./controllers" // 會去找controllers下的index.js
 // 路徑是資料夾 且 沒有檔名 就會去找該資料夾下的 index檔案（webpack慣例）
+
+import "./editor"
---
+++ app/javascript/editor.js
@@ -0,0 +1,5 @@
+import FroalaEditor from 'froala-editor'
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    let editor = new FroalaEditor('#post_content')
+})
